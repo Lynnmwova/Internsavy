99604f810b18a096dc0067434f4dd28161d7a61c
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2023-06-19 21:24:03 +0300

    tailwind form edits

--- contact-form/tailwind.config.js
+++ contact-form/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
+
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
